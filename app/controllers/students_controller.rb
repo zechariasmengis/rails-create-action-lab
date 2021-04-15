03a6efed9654a7ee9baeaf10df5d4de0715e3880
Zecharias Mengis <zmengis@gmail.com>
2021-04-15 14:59:53 -0500

    commit tree da5d0a372e3e7282f67d6742f0082e26f5f155ed

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,6 +12,13 @@ class StudentsController < ApplicationController
   end
 
   def create
+    @student = Student.create(
+      {
+        first_name: params[:first_name],
+        last_name: params[:last_name]
+      }
+    )
+    redirect_to student_path(@student)
   end
 
 end
